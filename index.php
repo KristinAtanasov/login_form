c05e1c889fd7161551ead007add4fff07f617609
K.Atanasov <67967349+KristinAtanasov@users.noreply.github.com>
2022-06-12 12:21:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +7,6 @@
         <link rel="stylesheet" href="css/style.css">
         <title>Document</title>
     </head>
-
     <body>
         <section>
             <div class="wraper">
@@ -38,5 +36,4 @@
             </div>
         </section>
     </body>
-
 </html>
